aed3ead115a4c44629432c58c5634f9883bc46fc
unknown <ismail.ouali@ump.ac.ma>
2024-02-02 11:14:53 +0100

    Adding the display of destinations in the form

---
+++ app/Http/Controllers/DestinationViewController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+use App\Http\Controllers\Controller;
+class DestinationViewController extends Controller
+{
+    //
+    public function index(){
+        $destination = DB::table('destination')
+        ->select("*")
+        ->get();
+    
+        return view('addAdv',['destination'=>$destination]);
+        }
+}

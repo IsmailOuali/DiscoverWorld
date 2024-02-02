5aaba035d09cab1e8fd1583da5a2033e326db455
unknown <ismail.ouali@ump.ac.ma>
2024-02-02 10:16:14 +0100

    Modifying thr form

--- app/Http/Controllers/advInsertController.php
+++ app/Http/Controllers/advInsertController.php
@@ -14,17 +14,18 @@ class AdvInsertController extends Controller
     }
 
         public function insert(Request $request){
-            $request->validate([
-                "image" => "required"
-            ]);
-        // $name = $request->input('name');
-        // $description = $request->input('description');
-        $image = $request->input('image');
+            // $request->validate([
+            //     "image" => "required"
+            // ]);
+        $name = $request->input('name');
+        $description = $request->input('description');
+        // $image = $request->input('image');
         $data=array(
-            // 'name'=>$name,"description"=>$description,
-        "image"=>$image);
-        DB::table('photo')->insert($data);
+            'name'=>$name,"description"=>$description,
+        // "image"=>$image
+    );
+        DB::table('aventure')->insert($data);
         echo "Record inserted successfully.<br/>";
-        // echo '<a href = "/insert">Click Here</a> to go back.';
+        echo '<a href = "/insert">Click Here</a> to go back.';
         }
         }
